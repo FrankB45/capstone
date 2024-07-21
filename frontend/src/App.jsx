a6771724eb501000a431d34bbb8d11223d5695c1
Frank <frankst3r@optonline.net>
2024-07-20 22:33:46 -0400

    Moved Components around. Added Font Awesome for Icons. Changed the SlideBar component so it could be used on both sides instead of just the left.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,49 @@
 import { useState } from 'react'
 import './App.css'
-import CountDown from './CountDown'
-import Sidebar from './SideBar'
-import SlideBar from './SlideBar'
+import CountDown from './components/CountDown'
+import SlideBar from './components/SlideBar'
+import Timings from './components/Timings'
 
 function App() {
 
   //Timeset for the countdown timer
   //Can be modified by the user via the SlideBar
   const [timeSet, setTimeSet] = useState([60, 30, 10, 90]);
+  const [offTimeSet, setOffTimeSet] = useState([0, 30, 10, 30]);
+
+  //Open state for the slidebars 
+  //Only want one open at a time so we will have to change state if both are open
+  const [isLeftOpen, setIsLeftOpen] = useState(true);
+  const [isRightOpen, setIsRightOpen] = useState(false);
+
+  const setIsOpen = (side) => {
+    if (side === 'left') {
+      if(isRightOpen) {
+        setIsRightOpen(false);
+      }
+      setIsLeftOpen(!isLeftOpen);
+    } else {
+      if(isLeftOpen) {
+        setIsLeftOpen(false);
+      }
+      setIsRightOpen(!isRightOpen);
+    }
+  };
+
+
 
   return (
     <div className='flex flex-row items-center flex-nowrap content-between'>
-      <SlideBar setTimeSet = {setTimeSet} />
+      <SlideBar isOpen={isLeftOpen} setIsOpen={setIsOpen} side='left'>
+        <Timings setTimeSet = {setTimeSet} />
+      </SlideBar>
       <div className='flex flex-col items-center flex-1 grow'> 
         <h1 style={{fontSize:'min(10vw, 10vh)'}} className=''>Arch Shot</h1>
         <CountDown timeSet={timeSet} />
       </div>
+      <SlideBar isOpen={isRightOpen} setIsOpen={setIsOpen} side='right'>
+        <h1>Sidebar</h1>
+      </SlideBar>
     </div>
   )
 }
---
+++ frontend/src/components/SlideBar.jsx
@@ -0,0 +1,27 @@
+import {React, useState} from 'react'
+import { Container, Row, Col, Button} from 'reactstrap'
+import Timings from './Timings'
+import './SlideBar.css'
+
+function SlideBar({ isOpen, setIsOpen, side = 'left', children }) {
+    //const [isOpen, setIsOpen] = useState(initiallyOpen);
+    //Converts side to boolean
+    const isLeft = side === 'left';
+
+    return (
+        <div className={`flex h-screen transition-all duration-300 ease-in-out ${isOpen ? 'w-1/4' : 'w-0'} ${isLeft ? 'flex-row' : 'flex-row-reverse'}`}>
+            <div className={`flex w-full`}>
+                <div className={`h-full w-full overflow-y-scroll no-scrollbar ${isOpen ? 'p-2' : 'p-0'}`}>
+                    {isOpen && <h2 className='text-lg font-bold'>Controls</h2>}
+                    {children}
+                </div>
+            </div>
+            <Button onClick={() => setIsOpen(side)} className='flex text-2xl font-extrabold bg-zinc-700 z-50'>{isOpen 
+    ? (isLeft ? '◀' : '▶') 
+    : (isLeft ? '▶' : '◀')
+}</Button>
+        </div>
+    )
+}
+
+export default SlideBar
