4941f35266e1d8856a3d6a1ac150426c3c199395
Frank <frankst3r@optonline.net>
2024-07-22 22:05:03 -0400

    Added CORS to get HTTP-only cookie working

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const userRoutes = require('./routes/userRoutes');
 const gameRoutes = require('./routes/gameRoutes');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 //Create express app
 const app = express();
@@ -12,6 +13,12 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use(cookieParser());
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
     res.json({ 'msg': 'Hello World' });
